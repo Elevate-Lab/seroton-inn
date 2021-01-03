798a5c921b7a2e928f806dab6cc5afb635c02891
Anjali <nova.anjali@gmail.com>
2021-01-03 17:22:39 +0530

    Added a little more content

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ function Home(){
     return(
         <div>
             <h1>Welcome to Seroton Inn</h1>
+            <p>This is a social media platform made for you and your mind. Mental Health is given priority over everything else. Seroton Inn was made to provide an Healthy, Safe and Toxic free environment to you!</p>
         </div>
     )
 }
